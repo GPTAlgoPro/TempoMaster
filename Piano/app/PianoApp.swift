64cae845fd9ad82e1f67ce8e7dc2afaa2786f846
Kai Sun <sunkai@dragonpass.com.cn>
2025-12-04 14:18:01 +0800

    small bug fixed

--- Piano/app/PianoApp.swift
+++ Piano/app/PianoApp.swift
@@ -2,9 +2,22 @@ import SwiftUI
 
 @main
 struct PianoApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             PianoMainView()
         }
+    }
+}
+
+// MARK: - App Delegate for Orientation Lock
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        supportedInterfaceOrientationsFor window: UIWindow?
+    ) -> UIInterfaceOrientationMask {
+        // 锁定为仅竖屏模式
+        return .portrait
     }
 }
